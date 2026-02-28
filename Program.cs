8504b217d9167ca1c8fd930b7340eb462d27cc18
zachschool1 <zacharyjordan@my.whccd.edu>
2026-02-27 19:25:57 -0800

    start with basic logic for menu and looping.

--- Program.cs
+++ Program.cs
@@ -4,9 +4,50 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //daily expense tracker
+            List<double> expenses = new List<double>();
 
-            Console.WriteLine("test change 1");
+            double currentExpense;
+
+            do
+            {
+                Console.WriteLine("1) Add New Expense.");
+                Console.WriteLine("2) Show all expenses.");
+                Console.WriteLine("3) Delete an expense.");
+                Console.WriteLine("4) Exit the program.");
+
+                string response = Console.ReadLine();
+
+                switch (response)
+                {
+                    case "1":
+                        //add new expense
+                        //expenses.Add(currentExpense);
+                        break;
+                    case "2":
+                        //show all expenses
+                        break;
+                    case "3":
+                        //show all expenses
+                        break;
+                    case "4":
+                        break;
+                    default:
+                        Console.Write("Please choose between option 1-4.");
+                        break;
+                }
+
+            } while (true);
+
+
+            
+
+
         }
+/*
+        static double getExpense(string expense)
+        {
+            Console.WriteLine("How much did you spend on food today?");
+        }*/
     }
 }
